22aa60bd5ab6d21ba94f887b236112e5bb083d18
Chinmayee Surwade <chinmayee.surwade@gmail.com>
2026-09-16 14:51:05 +0530

    Add portfolio root layout

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Chinmayee Surwade | Business & Data Analyst',
+  description: 'Business Analyst and Data Analyst portfolio featuring banking, consulting and analytics case studies.'
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
